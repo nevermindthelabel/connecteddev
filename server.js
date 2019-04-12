a6cf560d9f70a7964d91ad974649ffdf7dc1ed3a
Matt Kilcup <nevermindthelabel@gmail.com>
2019-04-12 04:57:31 -0700

    added passport

--- server.js
+++ server.js
@@ -2,25 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const routes = require('./routes');
+const passport = require('passport');
 
 const app = express();
-const PORT = process.env.PORT ||3001;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-};
+const PORT = process.env.PORT || 3001;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/connecteddevdb",
- { useNewUrlParser: true }, () => console.log('database connected'));
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/connecteddevdb',
+  { useNewUrlParser: true },
+  () => console.log('database connected')
+);
 
- const db = mongoose.connection
+const db = mongoose.connection;
 
- db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
