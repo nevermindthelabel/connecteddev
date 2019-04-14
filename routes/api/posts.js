68f8f2d8aad91e085f80e31f5edfc2cf696319e7
Matt Kilcup <nevermindthelabel@gmail.com>
2019-04-14 08:32:51 -0700

    added routes for posting and deleting comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -81,4 +81,49 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
   });
 });
 
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Post.findById(req.params.id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+      post.comments.unshift(newComment);
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({ commenterror: 'There is no post with this id.' }));
+});
+
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(comment => comment._id.toString() === req.params.comment_id)
+            .length === 0
+        ) {
+          return res.status(404).json({ commentnotthere: 'The comment does not exist.' });
+        }
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then((post = res.json(post)));
+      })
+      .catch(err => res.status(404).json({ commentnotfound: 'Comment not found.' }));
+  }
+);
+
 module.exports = router;
