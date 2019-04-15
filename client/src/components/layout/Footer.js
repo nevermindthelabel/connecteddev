d673f853b87e6a596c3c1429a1359864fb581c53
Matt Kilcup <nevermindthelabel@gmail.com>
2019-04-14 22:17:10 -0700

    added forms to register and login user

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default (props) => {
+export default props => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} Connected Dev
     </footer>
-  )
-}
+  );
+};
