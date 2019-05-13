1c2144875d1e5137646651a074f66e5dce138b6c
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-12 22:17:12 -0700

    added mongo URI connection

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const routes = require('./routes')
 
 
 const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send(`API running`));
+
+app.use(routes)
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => 
-  console.log(`Server listening on port http://${PORT}`));
+  console.log(`Server listening on port http://localhost:${PORT}`));
