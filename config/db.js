4f03807997e0a7b4dbcb0c221dab76b66f2e8025
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-12 22:18:02 -0700

    added mongoDB config files

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = process.env.mongoURI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
     console.log('mongoDB connected')
   } catch (err) {
     console.error(err.message)
