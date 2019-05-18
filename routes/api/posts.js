f6ca1eb6fc2a0aed1eea1af46dea32b7c4c89848
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-18 16:28:09 -0700

    added routes to add and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -107,7 +107,7 @@ router.put('/like/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-      return res.status(400).json({ msg: "Post already liked by this user" });
+      return res.status(400).json({ msg: 'Post already liked by this user' });
     }
     post.likes.unshift({ user: req.user.id });
 
@@ -126,21 +126,90 @@ router.put('/unlike/:id', auth, async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
-      return res.status(400).json({ msg: "You have not liked this post yet" });
+      return res.status(400).json({ msg: 'You have not liked this post yet' });
     }
     const removeLike = post.likes.map(like => like.user.id.toString()).indexOf(req.user.id);
 
-    post.likes.splice(removeLike, 1)
+    post.likes.splice(removeLike, 1);
 
     await post.save();
 
     res.json(post.likes);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Internal Server Error')
+    res.status(500).send('Internal Server Error');
   }
 });
 
+// POST route to comment on a post
 
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
+
+// Delete Remove a comment
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'No comment found' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not Authorized' });
+    }
+
+    const removeComment = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeComment, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
