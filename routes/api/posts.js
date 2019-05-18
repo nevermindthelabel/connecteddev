3e70a65a8e3c78e5c31bde1a1bf0e6e2f632406c
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-18 09:00:01 -0700

    route to create a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,47 @@
 const router = require('express').Router();
+const User = require('../../models/User');
+const Post = require('../../models/Posts');
+const Profile = require('../../models/Profile');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => res.send('Posts route'));
+// POST request for creating a post
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post ({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      })
+
+      const post = await newPost.save();
+
+      res.json(post);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
