c5e1f41cd279a383696949332593f5167a87b697
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-19 16:39:26 -0700

    added successful and failed login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 import setAlert from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from './types';
 import setAuthToken from '../utils/setAuthToken';
 
-export const loadUser = () => async dispatch =>  {
+export const loadUser = () => async dispatch => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
   }
@@ -12,13 +19,13 @@ export const loadUser = () => async dispatch =>  {
     dispatch({
       type: USER_LOADED,
       payload: res.data
-    })
+    });
   } catch (err) {
     dispatch({
       type: AUTH_ERROR
-    })
+    });
   }
-}
+};
 
 export const register = ({ name, email, password }) => async dispatch => {
   const config = {
@@ -44,6 +51,32 @@ export const register = ({ name, email, password }) => async dispatch => {
     }
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+};
+
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 };
