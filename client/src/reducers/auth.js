4d59c211cd13b93675bb111ccae8990cac484bd1
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-19 16:45:42 -0700

    added successfull and failed login functionality

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -17,9 +24,10 @@ export default function(state = initialState, action) {
         isAuthenticated: true,
         loading: false,
         user: payload
-      }
+      };
     }
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -28,7 +36,8 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
-      case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
@@ -39,4 +48,4 @@ export default function(state = initialState, action) {
     default:
       return state;
   }
-};
+}
