963c94b7f3a5fa93d7c479fa162d45333821d62c
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-21 21:43:38 -0700

    added custom dashboard page for user

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,28 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
-useEffect(() => {
-  getCurrentProfile();
-}, [])
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { loading, profile } }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <h2 className="text-success">Thank you for creating a profile {user.name}</h2>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <h2 className="text-danger">There is no Profile for this account, {user.name}</h2>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth,
   profile: state.profile
-})
+});
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
