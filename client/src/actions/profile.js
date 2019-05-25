4e6c32ee7b9eb21d4abfca7bdf8a46b01ee6ce33
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-25 11:06:06 -0700

    created add experience action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -24,34 +24,63 @@ export const createOrUpdateProfile = (formData, history, edit = false) => async 
       headers: {
         'Content-Type': 'application/json'
       }
-    }
+    };
     const res = await axios.post('/api/profile', formData, config);
 
-       dispatch({
-         type: GET_PROFILE,
-         payload: res.data
-       });
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
 
-       dispatch(
-         setAlert(edit ? 'Profile has been Updated' : 'Thank you, Profie Created', 'success')
-       );
+    dispatch(setAlert(edit ? 'Profile has been Updated' : 'Thank you, Profie Created', 'success'));
 
-       if (!edit) {
-         history.push('/dashboard');
-       }
-
+    if (!edit) {
+      history.push('/dashboard');
+    }
   } catch (err) {
-     const errors = err.response.data.errors;
+    const errors = err.response.data.errors;
 
-     if (errors) {
-       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-     }
-     
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
-
-   
   }
-}
+};
+
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+
