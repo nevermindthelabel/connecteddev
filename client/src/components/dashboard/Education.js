45a2f38c9d5e83d2386c0ac7775bf057061d2ca5
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-25 21:53:23 -0700

    added education table component

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+const Education = ({ education }) => {
+  const allEducation = education.map(edu => (
+    <tr key={edu._id}>
+      <td>{edu.school}</td>
+      <td className='hide-sm'>{edu.degree}</td>
+      <td>
+        <Moment format='YYYY/MM/DD'>{edu.from}</Moment> -{' '}{edu.to === null ? ' Now' : <Moment format={'YYYY/MM/DD'}>{edu.to}</Moment>}
+      </td>
+      <td></td>
+      <button className="btn btn-danger">Delete</button>
+    </tr>
+  ));
+  return (
+    <Fragment>
+      <h2 className="my2">Education Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>School</th>
+            <th className="hide-sm">Degree</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{allEducation}</tbody>
+      </table>
+    </Fragment>
+  )
+};
+
+Education.propTypes = {
+  education: PropTypes.array.isRequired
+};
+
+export default connect()(Education);
