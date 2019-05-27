01c7532db7333c2ff05c4cace13b23269e214e45
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-26 21:02:49 -0700

    initial skeleton for all profiles

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/spinner.gif';
+import { getAllProfiles } from '../../actions/profile';
+
+const Profiles = ({ getAllProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getAllProfiles();
+  }, []);
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Profiles.propTypes = {
+  getAllProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getAllProfiles })(Profiles);
