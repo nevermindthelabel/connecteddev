964790b725762919895d7a5cb92fe879f6dfa000
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-30 18:25:51 -0700

    added dummy content into components for rendering

---
+++ client/src/components/profile/ProfileEdu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileEdu = props => {
+  return (
+    <div>
+      Education
+    </div>
+  )
+}
+
+ProfileEdu.propTypes = {
+
+}
+
+export default ProfileEdu;
---
+++ client/src/components/profile/ProfileExp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileExp = props => {
+  return (
+    <div>
+      Experience
+    </div>
+  )
+}
+
+ProfileExp.propTypes = {
+
+}
+
+export default ProfileExp;
