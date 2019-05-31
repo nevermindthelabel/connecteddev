7b1dc07fad610d11d0f0f3128286f0c44da9abb9
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-30 20:38:16 -0700

    completed education component

--- client/src/components/profile/ProfileEdu.js
+++ client/src/components/profile/ProfileEdu.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import moment from 'moment';
 
-const ProfileEdu = props => {
+const ProfileEdu = ({ education: { school, degree, fieldofstudy, from, to, current, description } }) => {
   return (
     <div>
-      Education
+      <h3>{school}</h3>
+      <p>
+        <Moment format="YYYY/MM/DD">{moment.utc(from)}</Moment> -{' '}
+        {current ? 'Now' : <Moment format="YYYY/MM/DD">{moment.utc(to)}</Moment>}
+      </p>
+      <p>
+        <strong>Degree: </strong>{degree}
+      </p>
+      <p>
+        <strong>Field Of Study: </strong>{fieldofstudy}
+      </p>
+      <p>
+        <strong>Description: </strong>{description}
+      </p>
     </div>
-  )
+  );
 }
 
 ProfileEdu.propTypes = {
-
+  education: PropTypes.object.isRequired
 }
 
 export default ProfileEdu;
