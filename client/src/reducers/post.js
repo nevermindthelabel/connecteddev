5ddce645157a318e5282a6fcc3f33204861b1614
Matt Kilcup <nevermindthelabel@gmail.com>
2019-06-01 22:34:14 -0700

    added add and remove comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -36,6 +38,12 @@ export default function(state = initialState, action) {
         posts: [payload, ...state.posts],
         loading: false
       };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false
+      };
     case UPDATE_LIKES:
       return {
         ...state,
@@ -55,6 +63,13 @@ export default function(state = initialState, action) {
         posts: state.posts.filter(post => post._id !== payload),
         loading: false
       };
+      case REMOVE_COMMENT:
+        return {
+          ...state,
+          post: { ...state, comments: state.post.comments.filter(comment => comment._id !== payload)} ,
+        loading: false
+        
+        }
     case POST_ERROR:
       return {
         ...state,
