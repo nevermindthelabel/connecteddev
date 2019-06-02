20e6d13b810d697db6e04e3e7a66f215d2a20caa
Matt Kilcup <nevermindthelabel@gmail.com>
2019-06-01 21:34:11 -0700

    added action to get post by id

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from './types';
 
 export const getPosts = () => async dispatch => {
   try {
@@ -96,3 +96,22 @@ export const addPost = formData => async dispatch => {
     });
   }
 };
+
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+
+    dispatch(setAlert('There is no post with that Id', 'danger'));
+  }
+}
